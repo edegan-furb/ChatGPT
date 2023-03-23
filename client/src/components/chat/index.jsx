922f0156536a3c1ffec27cbeac5a0a905a40da34
EduDegan <edegan@furb.br>
2023-03-23 01:14:00 -0300

    message form

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -5,6 +5,7 @@ import {
     MultiChatWindow
 } from 'react-chat-engine-advanced';
 import Header from "@/components/customHeader";
+import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm"
 
 const Chat = () => {
     const chatProps = useMultiChatLogic(
@@ -13,14 +14,21 @@ const Chat = () => {
         "1234"
     )
 
-    return <div style={{ flexBasis: "100%" }}>
-        <MultiChatSocket{...chatProps} />
-        <MultiChatWindow
-            {...chatProps}
-            style={{ height: "100vh" }}
-            renderChatHeader={(chat) => <Header chat={chat} />}
-        />
-    </div>;
+    return (
+        <div style={{ flexBasis: "100%" }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow
+                {...chatProps}
+                style={{ height: "100vh" }}
+                renderChatHeader={(chat) => <Header chat={chat} />}
+                renderMessageForm={(props)=> {
+                    return(
+                        <StandardMessageForm props={props} activeChat={chatProps.chat}/>
+                    )
+                }}
+            />
+        </div>
+    );
 };
 
 export default Chat
